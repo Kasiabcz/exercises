d2078b91974af833ddabbc1479b40f697ac692cf
Kasia <Buczkowska.Katarzyna@wp.pl>
2019-12-10 23:55:38 +0100

    Removing unnecessary comment

--- optics/src/Diameter.java
+++ optics/src/Diameter.java
@@ -6,7 +6,6 @@ public class Diameter
     {
         FiFactory skanFactory = new ScanFiFactory();
         Fi diamData = skanFactory.createFi();
-        //diamData.skan(); !To już nie jest potrzebne!
         double diam = 2 * (diamData.getRmax() + (diamData.getPDop() / 2) - diamData.getWidth() + diamData.getSpace() +2);
         Result.choice(diam);
         System.out.println();
